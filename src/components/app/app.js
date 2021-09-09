ae2b6fa40ad5807e9424180055773f1eb8cc92c4
Roman Franko <roma.franko99@gmail.com>
2021-09-09 19:23:11 +0300

    Create footer

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,16 @@
 import React from "react"
 import Header from "../header/header";
 import Layout from "../layout/layout";
+import Footer from "../footer/footer";
 
 const App = () => {
     return (
         <>
             <Header title={"Title"} descr={"Description"}/>
             <Layout/>
+            <Layout/>
+            <Layout/>
+            <Footer/>
         </>
     )
 }
--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,3 +1,5 @@
+// import layout from "./layout.css"
+
 const Layout = () => {
     return (
         <section className="root">
