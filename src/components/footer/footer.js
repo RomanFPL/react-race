9c7c0d8bb590b3d41109022c4e86917bdbc54c03
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-10 09:34:59 +0300

    Chenge  setting bg or color

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import s from "./footer.module.css";
 const Footer = () => {
     return (
         <footer>
-            <div class={s.wrapper}>
+            <div className={s.wrapper}>
                 <h3>THANKS FOR VISITING</h3>
                 <p>© 2021 #ReactMarathon.</p>
             </div>
