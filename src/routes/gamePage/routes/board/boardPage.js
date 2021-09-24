0e7ce03563dc333d4acf9ff3e46acf1173b38286
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-24 15:16:43 +0300

    Add player2

--- src/routes/gamePage/routes/board/boardPage.js
+++ src/routes/gamePage/routes/board/boardPage.js
@@ -11,13 +11,18 @@ const BoardPage = () => {
 
     const history = useHistory();
     const [board, setBoard] = useState([])
+    const [player2, setPlayer2] = useState([])
 
-    console.log("###", board)
+    console.log("###", player2)
 
     useEffect(async () => {
         const boardResponse = await fetch("https://reactmarathon-api.netlify.app/api/board");
         const boardRequest = await boardResponse.json();
         setBoard(boardRequest.data)
+
+        const player2Response = await fetch("https://reactmarathon-api.netlify.app/api/create-player");
+        const player2Requerst = await player2Response.json();
+        setPlayer2(player2Requerst.data)
     }, [])
     // if(Object.keys(cards.pokemons).length === 0){
     //     history.replace("/game");
@@ -27,27 +32,31 @@ const BoardPage = () => {
         console.log("click!")
     }
 
+    player2.map(({id, type, values, img, name, selected}) => {
+        console.log(id, type, values, img, name, selected)
+    });
+
     const handleClickBoardPlate = (position) => {
         console.log(position)
     }
     return (
         <div className={s.root}>
-						<div className={s.playerOne}>
-                        {Object.entries(cards.pokemons).map(([key, {id, type, values, img, name, selected}]) => 
-                        <PokemonCard 
-                            key={key}
-                            type={type} 
-                            values={values} 
-                            img={img} 
-                            name={name} 
-                            id={id} 
-                            cardBG={cardBG} 
-                            isActive={true} 
-                            handleCardClick={() => handleClickCard(key)}
-                            isSelected={selected}
-                            minimize
-                            className={s.card}/>)}
-						</div>
+            <div className={s.playerOne}>
+                {Object.entries(cards.pokemons).map(([key, {id, type, values, img, name, selected}]) => 
+                    <PokemonCard 
+                        key={key}
+                        type={type} 
+                        values={values} 
+                        img={img} 
+                        name={name} 
+                        id={id} 
+                        cardBG={cardBG} 
+                        isActive={true} 
+                        handleCardClick={() => handleClickCard(key)}
+                        isSelected={selected}
+                        minimize
+                        className={s.card}/>)}
+            </div>
             <div className={s.board}>
                 {board.map(item => (
                     <div 
@@ -57,6 +66,22 @@ const BoardPage = () => {
                         {item.card && <PokemonCard {...item} minimize />}
                     </div>
                 ))}
+            </div>
+            <div className={s.playerTwo}>
+                {player2.map(({id, type, values, img, name, selected}) => 
+                            <PokemonCard 
+                                key={id}
+                                type={type} 
+                                values={values} 
+                                img={img} 
+                                name={name} 
+                                id={id} 
+                                cardBG={cardBG} 
+                                isActive={true} 
+                                handleCardClick={() => handleClickCard(id)}
+                                isSelected={selected}
+                                minimize
+                                className={s.card}/>)}
             </div>
         </div>
     );
