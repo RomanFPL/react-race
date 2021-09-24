17533532573cbf159c581229cef6570c138f694f
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-24 22:50:09 +0300

    Add enemy cards

--- src/routes/gamePage/routes/finish/index.js
+++ src/routes/gamePage/routes/finish/index.js
@@ -6,11 +6,12 @@ import s from "./style.module.css"
 
 const FinishPage = () => {
     const cards = useContext(PokemonContext);
-    console.log(cards)
+    // console.log(cards.pokemonsEnemy)
+    Object.entries(cards.pokemonsEnemy).map(x=> console.log(x))
     return (
         <div>
              <h1>This is Game Page!!!</h1>
-             <section className={s.player1}>
+             <section className={s.player}>
              {Object.entries(cards.pokemons).map(([key, {id, type, values, img, name, selected}]) => (
                  <div className={s.cardWrap}>
                      <PokemonCard 
@@ -26,6 +27,25 @@ const FinishPage = () => {
                          isSelected={selected}
                          minimize={null}
                          />
+                 </div>
+             ))}
+             </section>
+             <section className={s.player}>
+             {Object.entries(cards.pokemonsEnemy).map(([key, {id, type, values, img, name, selected}]) => (
+                 <div className={s.cardWrap}>
+                    <PokemonCard 
+                        key={id}
+                        type={type} 
+                        values={values} 
+                        img={img} 
+                        name={name} 
+                        id={id} 
+                        cardBG={cardBG} 
+                        isActive={true} 
+                        handleCardClick={null}
+                        isSelected={selected}
+                        minimize={null}
+                    />
                  </div>
              ))}
              </section>
