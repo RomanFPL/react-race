43861111f0b56d34bddb5e9d476a1b76782c705c
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-24 18:01:44 +0300

    Add class selection & method

--- src/routes/gamePage/routes/board/components/PlayerBoard/index.js
+++ src/routes/gamePage/routes/board/components/PlayerBoard/index.js
@@ -1,12 +1,20 @@
 import PokemonCard from "../../../../../../components/pokemonCard";
 import cardBG from "../../../../../../assets/cardBack.jpg"
 import s from "./style.module.css"
+import { useState } from "react";
+import cn from "classnames";
 
 const PlayerBoard = ({cards}) => {
+    const [isSelected, setSelected] = useState(null)
+
     return (
     <>
         {cards.map(({id, type, values, img, name, selected}) => (
-            <div className={s.cardBoard}>
+            <div className={cn(s.cardBoard, {
+                [s.selected]: isSelected === id
+            })}
+            onClick={() => {setSelected(id)}}
+            >
                 <PokemonCard 
                     key={id}
                     type={type} 
