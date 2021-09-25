28c647f3e698a19fe9b6552bcdb3b3b68333c598
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-25 18:58:45 +0300

    Add selected card to state

--- src/routes/gamePage/routes/finish/index.js
+++ src/routes/gamePage/routes/finish/index.js
@@ -4,22 +4,29 @@ import cardBG from "../../../../assets/cardBack.jpg"
 import { PokemonContext } from "../../../../services/pokemonContext";
 import s from "./style.module.css"
 import { useHistory } from "react-router";
+import cn from "classnames";
+import { useState } from "react/cjs/react.development";
 
 const FinishPage = () => {
     const cards = useContext(PokemonContext);
     const history = useHistory()
 
+    const [selectedCard, setSelectedCard] = useState({})
+    const [cardNum, setCardNum] = useState({})
+    
+
     if(!Object.values(cards.pokemons).length){
         history.replace("/game/");
     }
 
+    console.log(selectedCard);
     return (
         <div>
              <h1 className={s.btnCenter} style={{textAlign: "center"}}>Finish</h1>
              <h1 className={s.btnCenter} style={{textAlign: "center"}}>You win. Select one from enemy cards to add it to your deck.</h1>
              <section className={s.player}>
              {Object.entries(cards.pokemons).map(([key, {id, type, values, img, name, selected}]) => (
-                 <div className={s.cardWrap}>
+                 <div className={s.cardWrap} key={key}>
                      <PokemonCard 
                          key={key}
                          type={type} 
@@ -38,8 +45,8 @@ const FinishPage = () => {
              </section>
              <button className={s.btnCenter} onClick={() => history.replace("/game/")}> End game</button>
              <section className={s.player}>
-             {Object.entries(cards.pokemonsEnemy).map(([key, {id, type, values, img, name, selected}]) => (
-                 <div className={s.cardWrap}>
+             {Object.entries(cards.pokemonsEnemy).map(([key, {id, type, values, img, name, selected}], i) => (
+                 <div className={cn(s.cardWrap, {[s.selectedCard]: cardNum===i})} key={key}>
                     <PokemonCard 
                         key={id}
                         type={type} 
@@ -49,7 +56,10 @@ const FinishPage = () => {
                         id={id} 
                         cardBG={cardBG} 
                         isActive={true} 
-                        handleCardClick={null}
+                        handleCardClick={() => {
+                            setSelectedCard({id, type, values, img, name, selected});
+                            setCardNum(i);
+                        }}
                         isSelected={selected}
                         minimize={null}
                     />
