d494bd2f4330b1bc06802a9990a0b490459660db
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-09-28 21:02:34 +0300

    Add actionsCreator

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./app"
-import { createStore } from 'redux';
-import rootReducer from "./store/counter"
+import { bindActionCreators, createStore } from 'redux';
+import rootReducer, * as actions from "./store/counter"
 
 const store = new createStore(rootReducer);
-console.log("$$$$", "store", store.getState())
+console.log("$$$$", "store", store.getState());
+store.subscribe(() => console.log("$##$$", store.getState()))
+const {plusAction, minusAction} = bindActionCreators(actions, store.dispatch);
+
+plusAction(5)
+plusAction(3)
+plusAction(7)
+plusAction(3)
+minusAction(1)
 
 ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.querySelector("#root"))
