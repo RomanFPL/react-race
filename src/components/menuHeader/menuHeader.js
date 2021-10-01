ea894c0abb718891168d803e73a6f5227dbfc819
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-01 13:33:12 +0300

    Add login btn

--- src/components/menuHeader/menuHeader.js
+++ src/components/menuHeader/menuHeader.js
@@ -4,14 +4,23 @@ import { useState } from "react"
 
 const MenuHeader = ({bgActive}) => {
     const [menuState, setMenu] = useState(null);
+    const [isOpenModal, setOpenModal] = useState(null);
     const toggleMenuActive = () => {
         setMenu(!menuState);
+    }
+
+    const handleClickLogin = () => {
+        setOpenModal(prevState => !prevState);
     }
 
     return(
         <>
             <Menu menuActive={menuState} menuTriger={toggleMenuActive}/>
-            <NavBar menuActive={menuState} menuTriger={toggleMenuActive} bgActive={bgActive}/>
+            <NavBar 
+                menuActive={menuState} 
+                menuTriger={toggleMenuActive} 
+                bgActive={bgActive}
+                onClickLogin={handleClickLogin}/>
        </> 
     )
 }
