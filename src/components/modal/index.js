d46fd303cdeca9b28828960ba16d9c1e47a16d06
Roman Franko <66442095+RomanFPL@users.noreply.github.com>
2021-10-01 15:11:02 +0300

    Hide modalPanel onClick external

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,16 +1,31 @@
 import cn from "classnames";
+import { useRef } from "react";
 
 import s from "./style.module.css"
 
 const Modal = ({title, children, onCloseModal, isOpen}) => {
 
+    const modalEl = useRef();
+
     const handleCloseModal = () => {
         onCloseModal && onCloseModal(false)
     }
 
+    const onClickRoot = (e) => {
+        if(!modalEl.current.contains(e.target)) {
+            handleCloseModal();
+        }
+    }
+
     return (
-        <div className={cn(s.root, {[s.open] : isOpen})}>
-            <div className={s.modal}>
+        <div 
+            className=
+                {cn(s.root, 
+                {[s.open] : isOpen})}
+            onClick={onClickRoot}>
+            <div 
+                className={s.modal}
+                ref={modalEl}>
                 <div className={s.head}>
                     {title}
                     <span 
